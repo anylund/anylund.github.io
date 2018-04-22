00f0b51a28bf48c72aeee370c31183be581e380d
anylund <antti.nylund@tieto.com>
2018-04-22 17:21:57 +0300

    update

--- inject.js
+++ inject.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
   $('label,input').mousedown(function(){console.log(this.id)});
-  $('div.ui-radiobutton span').mousedown(function(){console.log($(this).parents('div.ui-radiobutton').get(0))});
+  $('div.ui-radiobutton span').mousedown(function(){
+    var rButton = $(this).parents('div.ui-radiobutton').get(0);
+    console.log(rbutton.id);
+  });
 });
