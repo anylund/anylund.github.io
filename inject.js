783e47ce779d7a8ec12be5240af29cc3e2364efa
anylund <antti.nylund@tieto.com>
2018-04-22 19:47:39 +0300

    update

--- inject.js
+++ inject.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   $('label,input').mousedown(function(){console.log(this.id)});
   $('div.ui-radiobutton span').mousedown(function(){
     var rButton = $(this).parents('div.ui-radiobutton').get(0);
-    console.log(rButton.id);
+    console.log('rButton ');
+    console.dir(rButton);
     if(rButton) {
       console.log('NO ID');
       var someID = rButton.parents('#id').get(0).id;
