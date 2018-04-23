67af71b428aa4f5f1f341f864f4da75240cee585
anylund <antti.nylund@tieto.com>
2018-04-23 18:03:34 +0300

    update

--- inject.js
+++ inject.js
@@ -6,22 +6,32 @@ $(document).ready(function() {
     while(!target.id){
       target = target.parentNode;
     }
-    console.log(target.id);
+    console.log('Dropdown '+target.id);
     fired = true;
   });
   $('ul.ui-selectcheckboxmenu-multiple-container,div.ui-selectcheckboxmenu-trigger').mousedown(function(e){
-    var rArrow = e.target.closest('div[id*=tat]');
-    console.log('Selection '+rArrow.id);
+    if(!fired) {
+      var target = e.target;
+      while(!target.id){
+        target = target.parentNode;
+      }
+      console.log('Dropdown multiple '+ target.id);
+    } else {
+      fired = 0;
+    }
   });
   
   $('span.ui-radiobutton-icon').mousedown(function(e){
-    var rButton = e.target.closest('table[id*=tat]');
-    console.log('Radiobutton '+rButton.id);
+    var target = e.target;
+    while(!target.id){
+      target = target.parentNode;
+    }
+    console.log('Radiobutton '+ target.id);
 
   });
   $('button,span.ui-button-text').mousedown(function(e){
     if(!fired) {
-      console.log(e.target.id);
+      console.log('Button '+e.target.id);
     } else {
       //Prevents button firing for the same event when drop down already fired
       fired = 0;
@@ -32,9 +42,10 @@ $(document).ready(function() {
     while(!target.id){
       target = target.parentNode;
     }
-    console.log(target.id);
-    //var tableId = e.target.closest('table[id*=tat]');
-    console.log('Table '+ target.id);
+    console.log('Checkbox '+ target.id);
+  });
+  $('a').mousedown(function(e){
+    console.log('Link '+e.innerText);
   });
 //Errors
   var errorLines = document.querySelectorAll('div.ui-messages-error > ul li');
